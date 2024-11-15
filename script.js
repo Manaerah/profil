fda8082d4b9f2769116c6f0b0e575bb0ae64be1a
Manaerah <nansilani96@gmail.com>
2024-11-15 03:41:18 -0700

    membuat biodata

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const { default: firebase } = require("firebase/compat/app");
-
 // Simple message submission handler
 function sendMessage(event) {
   event.preventDefault();
@@ -16,20 +14,3 @@ function sendMessage(event) {
   document.getElementById("email").value = "";
   document.getElementById("message").value = "";
 }
-const firebaseConfig = {
-  apiKey: "AIzaSyByrhYEc7w0Bp1p2-bgyhOr_xTj4jzVhD0",
-  authDomain: "my-profil-44cf2.firebaseapp.com",
-  databaseURL:
-    "https://my-profil-44cf2-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "my-profil-44cf2",
-  storageBucket: "my-profil-44cf2.firebasestorage.app",
-  messagingSenderId: "174741649026",
-  appId: "1:174741649026:web:3cf4a073ef967a0757269b",
-  measurementId: "G-2TGY25SZ64",
-};
-
-//initialize firebase//
-firebase.initializeApp(firebaseConfig);
-
-//reference your database//
-var myprofilDB = firebase.database().ref("my-profilForm");
